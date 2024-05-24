3319efb543d92794fa3543b9ee62b57871649105
Dylan Revsine <dylan.revsine@gmail.com>
2024-05-24 13:10:31 -0400

    setup file needed to initialize the package

---
+++ PhoneBook/__init__.py
@@ -0,0 +1,9 @@
+"""PhoneBook package initializer."""
+
+import flask
+
+app = flask.Flask(__name__)
+app.config.from_object('PhoneBook.config')
+app.config.from_envvar('PHONEBOOK_SETTINGS', silent=True)
+import PhoneBook.views
+import PhoneBook.model
