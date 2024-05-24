d60fafa2971384b94985b350404a6beaafba01f6
Dylan Revsine <dylan.revsine@gmail.com>
2024-05-24 13:09:47 -0400

    SQL database needed to store user login information and each users' contacts

---
+++ sql/schema.sql
@@ -0,0 +1,17 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    username VARCHAR(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY(username)
+);
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    contact_owner VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    email_address VARCHAR(255) NOT NULL,
+    home_address VARCHAR(255) NOT NULL,
+    FOREIGN KEY(contact_owner) REFERENCES users(username) ON DELETE CASCADE
+);
