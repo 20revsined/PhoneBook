bd8433e8b4e676df2315659a529a4969c16a1fc0
Dylan Revsine <dylan.revsine@gmail.com>
2024-05-25 16:01:43 -0400

    allow for profile pictures to be displayed in the phone book

--- PhoneBook/config.py
+++ PhoneBook/config.py
@@ -9,6 +9,9 @@ APPLICATION_ROOT = '/'
 SECRET_KEY = b"\x0b\x7f\x89\xb6P\x90\x13\x89\x92\x86\xf5rS\xff\x92\xb6\xaeF\xae;\x9fSpQ"
 SESSION_COOKIE_NAME = 'login'
 PHONEBOOK_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = PHONEBOOK_ROOT/'var'/'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
 # Database file is var/phonebook.sqlite3
 DATABASE_FILENAME = PHONEBOOK_ROOT/'var'/'phonebook.sqlite3'
