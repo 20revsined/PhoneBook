de1ed302117b5c549a4c35c204cbb24d2ad3a6e7
Dylan Revsine <dylan.revsine@gmail.com>
2024-05-27 12:06:14 -0400

    better style practices

--- PhoneBook/__init__.py
+++ PhoneBook/__init__.py
@@ -5,5 +5,6 @@ import flask
 app = flask.Flask(__name__)
 app.config.from_object('PhoneBook.config')
 app.config.from_envvar('PHONEBOOK_SETTINGS', silent=True)
+
 import PhoneBook.views
 import PhoneBook.model
