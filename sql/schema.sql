f852aadc2790e91937e9a9f64c59273d7addf9ec
Dylan Revsine <dylan.revsine@gmail.com>
2024-05-27 23:57:14 -0400

    added first_name and last_name columns to allow the app to greet the user by first and last name, rather than by user name

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,6 +3,8 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password varchar(255) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
     PRIMARY KEY(username)
 );
 
